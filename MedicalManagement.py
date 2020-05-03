9b3f7d07846cf62c16a9c37c6ee26edf5b683d17
Manoj <52457113+ManojAvarur@users.noreply.github.com>
2020-05-03 05:40:20 +0530

    My First Commit

--- MedicalManagement.py
+++ MedicalManagement.py
@@ -16,13 +16,13 @@ def bloodGroup( window ):
 
 def gui( window ):
 
-# ---------------------- Heading Row ---------------------------------------
+# ---------------------------------- Heading Row ----------------------------------------------------------------
     lbl = Label(window, text = "Enter Patient Details",font = ("Times New Roman", 26))
-    lbl.place(x = 385, y = 10)
+    lbl.place(x = 395, y = 10)
     lbl = Label(window, text = "----------------------------------",font = ("Times New Roman", 26))
-    lbl.place(x = 340, y = 50, height = 25)
+    lbl.place(x = 350, y = 50, height = 25)
 
-# ---------------------- First Row ---------------------------------------
+# ---------------------------------- First Row ------------------------------------------------------------------
     lbl = Label(window, text = "Patient ID : ", font = ("Times New Roman", 20))
     lbl.place(x = 102, y = 90)
     txtfld_0 = Entry(window, bd = 1.5,  font = ("Times New Roman", 16))
@@ -35,7 +35,7 @@ def gui( window ):
     txtfld_1 = Entry(window, bd = 1.5, font = ("Times New Roman", 16))
     txtfld_1.place(x = 790, y = 98, height = 25, width = 200)
 
-# ---------------------- Second Row ---------------------------------------
+# ---------------------------------- Second Row -----------------------------------------------------------------
     lbl = Label(window, text = "Phone No : ", font = ("Times New Roman", 20))
     lbl.place(x = 105, y = 150)
     txtfld_2 = Entry(window, bd = 1.5, font = ("Times New Roman", 16))
@@ -43,58 +43,98 @@ def gui( window ):
 
     lbl = Label(window, text = "Blood Group : ", font = ("Times New Roman", 20))
     lbl.place(x = 620, y = 150)
-    # txtfld2 = Entry(window, bd = 1.5, font = ("Times New Roman", 16))
-    # txtfld2.place(x = 720, y = 158, height = 25, width = 200)
     bloodGroup( window )
 
-# ---------------------- Third Row (Sugar Tests) ----------------------------------------
-    lbl = Label(window, text = "FBC : ", font = ("Times New Roman", 20))
+# ---------------------------------- Third Row (Sugar Tests) ----------------------------------------------------
+    lbl = Label(window, text = "FBS : ", font = ("Times New Roman", 20))
     lbl.place(x = 165, y = 210)
     txtfld_3 = Entry(window, bd = 1.5, font = ("Times New Roman", 16))
     txtfld_3.place(x = 240, y = 218, height = 25, width = 200)
+    lbl = Label(window, text = " mg/dl", font = ("Open Sans",10))
+    lbl.place(x = 440, y = 225) 
 
     lbl = Label(window, text = "PPBS : ", font = ("Times New Roman", 20))
     lbl.place(x = 700, y = 210)
     txtfld_4 = Entry(window, bd = 1.5, font = ("Times New Roman", 16))
     txtfld_4.place(x = 790, y = 218, height = 25, width = 200)
+    lbl = Label(window, text = " mg/dl", font = ("Open Sans",10))
+    lbl.place(x = 990, y = 225)
 
-# ---------------------- Forth Row ----------------------------------------
+# ---------------------------------- Forth Row ------------------------------------------------------------------
     lbl = Label(window, text = "Hypertension (BP) : ", font = ("Times New Roman", 20))
     lbl.place(x = 12, y = 270)
     txtfld_5 = Entry(window, bd = 1.5, font = ("Times New Roman", 16))
     txtfld_5.place(x = 240, y = 278, height = 25, width = 200)
+    lbl = Label(window, text = " mm Hg", font = ("Open Sans",10))
+    lbl.place(x = 440, y = 285)
 
-    lbl = Label(window, text = "Disease Type : ", font = ("Times New Roman", 20))
-    lbl.place(x = 588, y = 270)
+    lbl = Label(window, text = "CBC : ", font = ("Times New Roman", 20))
+    lbl.place(x = 710, y = 270)
     txtfld_6 = Entry(window, bd = 1.5, font = ("Times New Roman", 16))
     txtfld_6.place(x = 790, y = 278, height = 25, width = 200)
 
-# ---------------------- Fifth Row ----------------------------------------
-    lbl = Label(window, text = "Disease Type : ", font = ("Times New Roman", 20))
-    lbl.place(x = 42, y = 330)
+# ---------------------------------- Fifth Row ------------------------------------------------------------------
+    lbl = Label(window, text = "Thyroid : ", font = ("Times New Roman", 20))
+    lbl.place(x = 130, y = 330)
+
+    lbl = Label(window, text = "T3 = ", font = ("Times New Roman", 16))
+    lbl.place(x = 260, y = 334)
     txtfld_7 = Entry(window, bd = 1.5, font = ("Times New Roman", 16))
-    txtfld_7.place(x = 240, y = 338, height = 25, width = 200)
+    txtfld_7.place(x = 320, y = 338, height = 25, width = 100)
+    lbl = Label(window, text = " ng/dL", font = ("Open Sans",10))
+    lbl.place(x = 420, y = 341)
 
-    lbl = Label(window, text = "Disease Type : ", font = ("Times New Roman", 20))
-    lbl.place(x = 568, y = 330)
-    txtfld_8 = Entry(window, bd = 1.5, font = ("Times New Roman", 16))
-    txtfld_8.place(x = 790, y = 338, height = 25, width = 200)
-    
-# ---------------------- Sixth Row ----------------------------------------
-    lbl = Label(window, text = "Disease Type : ", font = ("Times New Roman", 20))
-    lbl.place(x = 62, y = 390)
+    lbl = Label(window, text = "T4 = ", font = ("Times New Roman", 16))
+    lbl.place(x = 510, y = 334)
     txtfld_7 = Entry(window, bd = 1.5, font = ("Times New Roman", 16))
-    txtfld_7.place(x = 240, y = 398, height = 25, width = 200)
+    txtfld_7.place(x = 570, y = 338, height = 25, width = 100)
+    lbl = Label(window, text = " mIU/L", font = ("Open Sans",10))
+    lbl.place(x = 670, y = 341)
 
-    lbl = Label(window, text = "Disease Type : ", font = ("Times New Roman", 20))
-    lbl.place(x = 588, y = 390)
-    txtfld_8 = Entry(window, bd = 1.5, font = ("Times New Roman", 16))
-    txtfld_8.place(x = 790, y = 398, height = 25, width = 200)
+    lbl = Label(window, text = "TSH = ", font = ("Times New Roman", 16))
+    lbl.place(x = 770, y = 334)
+    txtfld_7 = Entry(window, bd = 1.5, font = ("Times New Roman", 16))
+    txtfld_7.place(x = 840, y = 338, height = 25, width = 100)
+    lbl = Label(window, text = " mIU/L", font = ("Open Sans",10))
+    lbl.place(x = 940, y = 341)
 
+# ---------------------------------- Sixth Row ------------------------------------------------------------------
+    lbl = Label(window, text = "LFT : ", font = ("Times New Roman", 20))
+    lbl.place(x = 170, y = 390)
+
+    lbl = Label(window, text = "AST = ", font = ("Times New Roman", 16))
+    lbl.place(x = 255, y = 394)
+    txtfld_7 = Entry(window, bd = 1.5, font = ("Times New Roman", 16))
+    txtfld_7.place(x = 320, y = 398, height = 25, width = 100)
+    lbl = Label(window, text = " IU/L", font = ("Open Sans",10))
+    lbl.place(x = 420, y = 405)
+
+    lbl = Label(window, text = "ALT = ", font = ("Times New Roman", 16))
+    lbl.place(x = 505, y = 394)
+    txtfld_7 = Entry(window, bd = 1.5, font = ("Times New Roman", 16))
+    txtfld_7.place(x = 570, y = 398, height = 25, width = 100)
+    lbl = Label(window, text = " IU/L", font = ("Open Sans",10))
+    lbl.place(x = 670, y = 405)
+
+    lbl = Label(window, text = "ALP = ", font = ("Times New Roman", 16))
+    lbl.place(x = 770, y = 394)
+    txtfld_7 = Entry(window, bd = 1.5, font = ("Times New Roman", 16))
+    txtfld_7.place(x = 840, y = 398, height = 25, width = 100)
+    lbl = Label(window, text = " IU/L", font = ("Open Sans",10))
+    lbl.place(x = 940, y = 405)
 
 window = Tk()
 window.resizable(0,0)
 gui(window)
+
+Btn = Button(window, text ="Submit", bd = 6, font = ("Open Sans", 16))
+Btn.pack()
+Btn.place(x = 380, y = 460, height = 50, width = 100)
+
+Btn = Button(window, text ="Reset", bd = 6, font = ("Open Sans", 16))
+Btn.pack()
+Btn.place(x = 580, y = 460, height = 50, width = 100)
+
 window.title("Patient Medical Report")
-window.geometry("1050x700")
+window.geometry("1050x550")
 window.mainloop()
